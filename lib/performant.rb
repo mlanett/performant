ccf4e008cb7b92de2540366c52fb39a7b17d9378
Mark Lanett <mark.lanett@gmail.com>
2012-02-25 20:31:10 -0800

    Default configuration support.

--- lib/performant.rb
+++ lib/performant.rb
@@ -3,3 +3,4 @@ require "performant/version"
 module Performant
   # Your code goes here...
 end
+  autoload :Configuration,  "performant/configuration"
--- lib/performant/configuration.rb
+++ lib/performant/configuration.rb
@@ -1,6 +1,10 @@
 module Performant
 class Configuration
 
+  def self.default
+    Configuration.new
+  end
+
   attr :interval_size
 
   def initialize( options = {} )
@@ -13,6 +17,21 @@ class Configuration
     start  = Time.at( time.to_i - time.to_i % interval_size )
     finish = start + interval_size
     (start ... finish)
+  end
+
+  module Configurable
+    def self.included( it )
+      it.send( :attr, :configuration )
+      it.define_method( :configuration ) do
+        # auto-select default configuration if none present
+        @configuration ||= Configuration.default
+      end
+      it.define_method( :configuration= ) do |new_configuration|
+        # allow configuration to be changed, *once*
+        raise if @configuration || ! new_configuration
+        @configuration = new_configuration
+      end
+    end
   end
 
 end
