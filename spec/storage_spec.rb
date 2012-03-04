b2cca2aa829ece3cc409d19010f5d36ebfa6cdb3
Mark Lanett <mark.lanett@gmail.com>
2012-03-04 12:49:29 -0800

    clarify

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -9,29 +9,29 @@ describe Performant::Storage, redis: true, redis_configuration: true do
     subject.sample.should eq( { jobs: 0, busy: 0.0, work: 0.0 } )
 
     now = Time.at( 1330220626 )
-    subject.record_start( "test", time: now )
+    subject.record_start( "a", time: now )
     subject.sample.should eq( { jobs: 1, busy: 0.0, work: 0.0 } )
   end
 
   it "should record start events" do
     now = Time.at( 1330220626 )
-    subject.record_start( "test", time: now )
+    subject.record_start( "a", time: now )
     subject.sample[:jobs].should eq( 1 )
   end
 
   it "should record one operation" do
     now = Time.at( 1330220626 )
-    subject.record_start( "test", time: now )
-    subject.record_finish( "test", time: now + 1 )
+    subject.record_start( "a", time: now )
+    subject.record_finish( "a", time: now + 1 )
     subject.sample.should eq( { jobs: 0, busy: 1.0, work: 1.0 } )
   end
 
   it "should record sequential operations" do
     now = Time.at( 1330220626 )
-    subject.record_start( "test", time: now )
-    subject.record_finish( "test", time: now + 1 )
-    subject.record_start( "test", time: now + 2 )
-    subject.record_finish( "test", time: now + 3 )
+    subject.record_start( "a", time: now )
+    subject.record_finish( "a", time: now + 1 )
+    subject.record_start( "b", time: now + 2 )
+    subject.record_finish( "b", time: now + 3 )
     subject.sample.should eq( { jobs: 0, busy: 2.0, work: 2.0 } )
   end
 
