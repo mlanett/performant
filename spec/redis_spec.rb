1314ac493bdbaf9406d4fb4a5b81745547615f0b
Mark Lanett <mark.lanett@gmail.com>
2012-03-03 23:21:19 -0800

    Spec sorted sets so I know they work they way I expect.

--- spec/redis_spec.rb
+++ spec/redis_spec.rb
@@ -7,4 +7,14 @@ describe Redis, redis: true, redis_configuration: true do
     redis.should be_kind_of(Redis)
   end
 
+  it "has sorted sets" do
+    redis.zcard("foo").should eq(0)
+    redis.zadd("foo", 2, "bar")
+    redis.zadd("foo", 3, "goo")
+    redis.zcard("foo").should eq(2)
+    redis.zrange("foo", 0, 0).should eq(["bar"]) # 0th item = smallest item
+    redis.zrangebyscore( "foo", 0, 1, limit: [0,1] ).should eq([])
+    redis.zrangebyscore( "foo", 0, 2, limit: [0,1] ).should eq(["bar"])
+  end
+
 end
