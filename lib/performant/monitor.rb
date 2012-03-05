02b58d7d60bbc12dab46f039ebcb5e8090ff29bb
Mark Lanett <mark.lanett@gmail.com>
2012-03-04 19:02:49 -0800

    Prove monitor records starts and stops

--- lib/performant/monitor.rb
+++ lib/performant/monitor.rb
@@ -19,6 +19,15 @@ class Monitor
       @kind    = kind
     end
 
+    def track( &block )
+      id = start
+      begin
+        yield
+      ensure
+        finish( id )
+      end
+    end
+
     # creates a job id and records it starting
     # returns the job id
     def start( time = Time.now )
@@ -31,22 +40,13 @@ class Monitor
       storage.uninterruptedly { storage.record_finish( id, time: time ) }
     end
 
-    def track( kind = "test", timeout = 60, &block )
-      id = start
-      begin
-        yield
-      ensure
-        finish( id ) if id
-      end
-    end
-
     #
     protected
     #
 
     def make_job_id( time = Time.now )
       @hostname ||= `hostname`.chomp
-      "%s:%d:%s:%s:%f" % [ @hostname, Process.pid, Thread.current.object_id.to_s(36), Fiber.current.object_id.to_s(36), time.to_f ]
+      "%s@%s:%d:%s:%s:%f" % [ @kind, @hostname, Process.pid, Thread.current.object_id.to_s(36), Fiber.current.object_id.to_s(36), time.to_f ]
     end
 
   end # Client
--- spec/monitor_spec.rb
+++ spec/monitor_spec.rb
@@ -2,4 +2,14 @@
 require "helper"
 
 describe Performant::Monitor, redis: true, redis_configuration: true do
+
+  subject { Performant.monitor("test") }
+
+  it "records starts and stops" do
+    subject.track do
+      subject.storage.sample[:jobs].should eq(1)
+    end
+    subject.storage.sample[:jobs].should eq(0)
+  end
+
 end
