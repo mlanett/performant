a1b2d254a34ad55ad1665eeb7402289fff78b1f3
Mark Lanett <mark.lanett@gmail.com>
2012-03-25 22:53:50 -0700

    it can sample

--- lib/performant/sampler.rb
+++ lib/performant/sampler.rb
@@ -6,22 +6,34 @@ class Sampler
 
   include Configuration::Configurable
 
+  attr :jobs
+
   def initialize( jobs )
-    @jobs = jobs
+    @jobs   = jobs.freeze
+    @stores = jobs.inject({}) { |a,job| a[job] = Performant.storage(job); a }
   end
 
-  def sample( interval )
-    # read current totals from redis
-    # calculate deltas since last time
-    # write them into the interval
-    # roll up the interval as necessary
+  def run
+    loop_until_false do
+      puts Time.now
+      jobs.each do |job|
+        x = sample(job)
+        puts x.inspect
+      end
+      true
+    end
   end
 
-  def next_sample
-    # next minute
-    now = Time.now
-    Time.at( now.to_i + configuration.interval_size - now.to_i % configuration.interval_size )
+  def sample!( job, time = Time.now )
+    storage = store(job)
+    last    = storage.get_sample
+    current = storage.robustly { storage.tick!(time) }
+    diff    = diff( current, last )
+    storage.save_sample( current )
+    diff.merge( job: job )
   end
+
+  private
 
   def loop_until_false( &block )
     next_time = next_sample
@@ -33,18 +45,29 @@ class Sampler
     end
   end
 
+  # @returns next minute, or so
+  def next_sample
+    now = Time.now
+    Time.at( now.to_i + configuration.interval_size - now.to_i % configuration.interval_size )
+  end
+
   # sleeps until next time; tries to wake up *right* after the given time.
   def wait_until( time )
     while (d = time - Time.now) > 0 do
-      sleep( d / 2.0 )
+      sleep( d * 0.8 )
     end
   end
 
-  def diff( sample2, sample1 )
-    jobs = (sample2[:jobs] || 0)
-    busy = (sample2[:busy] || 0) - (sample1[:busy] || 0)
-    work = (sample2[:work] || 0) - (sample1[:work] || 0)
-    return { jobs: jobs, busy: busy, work: work }
+  def diff( b, a )
+    jobs   = (b[:jobs] || 0).to_i
+    busy   = (b[:busy] || 0.0) - (a[:busy] || 0.0)
+    work   = (b[:work] || 0.0) - (a[:work] || 0.0)
+    starts = ((b[:starts] || 0) - (a[:starts] || 0)).to_i
+    return { jobs: jobs, busy: busy, work: work, starts: starts }
+  end
+
+  def store( s )
+    @stores[s] or raise "No Tracking #{s}"
   end
 
 end # Sampler
--- spec/sampler_spec.rb
+++ spec/sampler_spec.rb
@@ -2,5 +2,27 @@
 require "helper"
 
 describe Performant::Sampler, redis: true, redis_configuration: true do
-  it "should have tests"
+
+  let( :storage ) { Performant.storage("test") }
+  subject { Performant.sampler(["test"]) }
+
+  describe "when sampling" do
+
+    it "can get a sample" do
+      storage.get_sample.should eq( { jobs: 0, busy: 0.0, work: 0.0, starts: 0 } )
+    end
+
+    it "can save the current diff" do
+      now = Time.at( 1000000000 )
+      subject.sample!("test",now).should eq( { jobs: 0, busy: 0.0, work: 0.0, starts: 0, job: "test" } )
+      storage.record_start( "a", time: now )
+      storage.record_finish( "a", time: now + 1 )
+      subject.sample!("test").should eq( { jobs: 0, busy: 1.0, work: 1.0, starts: 1, job: "test" } )
+    end
+
+    it "lists all monitored jobs"
+    # Performant:Jobs              < Set < Job > >
+
+  end # when sampling
+
 end
